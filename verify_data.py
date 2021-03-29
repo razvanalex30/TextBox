562873fefc8b96615fc1adfcbedd3ab66749fdd1
razvanalex30 <razvan_alex97@yahoo.com>
2021-03-29 13:56:02 +0300

    modified verify_data

--- verify_data.py
+++ verify_data.py
@@ -15,30 +15,4 @@ class VerifyData:
             print(f"{labels[0]} was completed!")
         
         
-    # def find_name(self):
-        # self.driver = SeleniumLibraryExt.create_driver()
-        # output_name = self.driver.find_element_by_xpath("//*[@id='output']/div//p[@id='name']")
-        # name = output_name.text
-        # if name != "":
-            # print("Name was found!")
-            #
-    # def find_email(self):
-        # self.driver = SeleniumLibraryExt.create_driver()
-        # output_email = self.driver.find_element_by_xpath("//*[@id='output']/div//p[@id='email']")
-        # email = output_email.text
-        # if email != "":
-            # print("Email was found!")
-            #
-    # def find_current_address(self):
-        # self.driver = SeleniumLibraryExt.create_driver()
-        # output_current_address = self.driver.find_element_by_xpath("//*[@id='output']/div//p[@id='currentAddress']")
-        # current_address = output_current_address.text
-        # if current_address != "":
-            # print("Current Address was found!")
-            #
-    # def find_permanent_address(self):
-        # self.driver = SeleniumLibraryExt.create_driver()
-        # output_permanent_address = self.driver.find_element_by_xpath("//*[@id='output']/div//p[@id='permanentAddress']")
-        # permanent_address = output_permanent_address.text
-        # if permanent_address != "":
-            # print("Permanent Address was found!")
+    
